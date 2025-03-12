fa0de2bc88b52dcc2678f70ab1304ab0c583c9ee
GEORGE-Ta <gaivrt@outlook.com>
2025-03-12 11:16:54 +0800

    change name to better-mermaid

--- main.ts
+++ main.ts
@@ -20,6 +20,7 @@ export default class MyPlugin extends Plugin {
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
+			new Notice('Here I am! GAIVR!')
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
